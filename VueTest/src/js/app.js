5641d60055928f1795c0249c9d89e13d63efd751
TurnerXi <290944015@qq.com>
2017-09-16 01:50:53 +0800

    vue test component

--- VueTest/src/js/app.js
+++ VueTest/src/js/app.js
@@ -1,9 +1,22 @@
 Vue.component('show-more-component', {
+	props:{
+		tools : {
+			type : Array,
+			default : function(){
+				return [];
+			}
+		}
+	},
 	template: '#show-more-template',
 	data: function () {
 	    return {
 	      show: false
 	    }
+	},
+	methods: {
+	    toolsEvent:function(key){
+	      	this.$emit('tools-taped',key);
+	      }
 	},
   	mounted :function(){
 	  	this.$nextTick(function(){
@@ -15,20 +28,42 @@ Vue.component('show-more-component', {
    	}
 })
 
-Vue.component('header-compontent', {
-  template: '#header-template'
+Vue.component('header-component', {
+	props: {currentPlay : {
+		type:Object,
+		default:function(){
+			return {
+				type : 1001,
+    			name : "普通"
+			}
+		}
+	}},
+	data:function(){
+		return {
+			show_nav : false,
+			tools : [
+				{key:"header_explain",text:"玩法说明"},
+				{key:"history",text:"历史走势"},
+				{key:"choose_random",text:"多期机选"}
+			]
+		}
+	},
+  	template: '#header-template',
+  	methods : {
+  		toolsTaped : function(key){
+			console.log(key);
+  		}
+  	}
 })
 
 var app = new Vue({
     el: '#selhm',
 	data: {
-    	play: {
+    	currentPlay : {
     		type : 1001,
     		name : "普通"
     	},
     	show_nav:false
   	}
 })
-
-
 
