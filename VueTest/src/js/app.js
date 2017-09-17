6d9d8aeb57d25f944c5c9e510c93409f751840c1
TurnerXi <290944015@qq.com>
2017-09-18 01:53:45 +0800

    vue test

--- VueTest/src/js/app.js
+++ VueTest/src/js/app.js
@@ -26,21 +26,35 @@ Vue.component('show-more-component', {
 		    });
 		});
    	}
-})
+});
+
+Vue.component('switch-play-component',{
+	props: {
+		currentPlay : {
+			type:Object,
+			default:function(){
+				return play_types[0];
+			}
+		},
+		show_play_box: false,
+		current_play_type : false
+	},
+	template:"#switch-play-template",
+	methods : {
+		switch_play:function(play_type){
+			this.$emit('switch-play-type',play_type);
+		},
+		is_current_play_type : function(play_type){
+			return this.current_play_type == play_type;
+		}
+	}
+});
 
 Vue.component('header-component', {
-	props: {currentPlay : {
-		type:Object,
-		default:function(){
-			return {
-				type : 1001,
-    			name : "普通"
-			}
-		}
-	}},
 	data:function(){
 		return {
-			show_nav : false,
+			currentPlay : play_types[0],
+			is_show_play : false,
 			tools : [
 				{key:"header_explain",text:"玩法说明"},
 				{key:"history",text:"历史走势"},
@@ -52,17 +66,31 @@ Vue.component('header-component', {
   	methods : {
   		toolsTaped : function(key){
 			console.log(key);
+  		},
+  		switchPlayType : function(playType){
+  			this.currentPlay = playType;
+  			this.is_show_play = !this.is_show_play;
   		}
-  	}
-})
+  	},
+  	computed: {
+	    currentPlayText: function () {
+	      return lot_name+"-"+this.currentPlay.p_name.substring(0,2);
+    	}
+    }
+});
 
+
+Vue.component('open-time-component',{
+	data : function(){
+		var current_issue = data_2004.current;
+		setInterval(function(){current_issue.accmulate_prize++},1000);
+		return current_issue;
+	},
+	template : "#open_time_template"
+});
 var app = new Vue({
     el: '#selhm',
 	data: {
-    	currentPlay : {
-    		type : 1001,
-    		name : "普通"
-    	},
     	show_nav:false
   	}
 })
