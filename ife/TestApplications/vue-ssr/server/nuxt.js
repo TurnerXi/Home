0390281a39cb1a23e6c0654e182ee46c89f56258
席平川 <pingchuan.xi@xiaomicp.com>
2018-01-10 21:10:13 +0800

    koa-vue

---
+++ ife/TestApplications/vue-ssr/server/nuxt.js
@@ -0,0 +1,24 @@
+import { Nuxt, Builder } from 'nuxt'
+
+export default function (app) {
+
+  return new Promise(function (resolve, reject) {
+    // Import and Set Nuxt.js options
+    let config = require('../nuxt.config.js')
+    config.dev = !(app.env === 'production')
+
+    // Instantiate nuxt.js
+    const nuxt = new Nuxt(config)
+
+    // Build in development
+    if (config.dev) {
+      const builder = new Builder(nuxt)
+      new Builder(nuxt).build().then(function () {
+        resolve(nuxt)
+      })
+    } else {
+      resolve(nuxt)
+    }
+  })
+
+}
