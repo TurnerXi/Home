9616bf9676c46aaf4b155dad9309a9d4edbce446
TurnerXi <290944015@qq.com>
2018-01-25 23:04:27 +0800

    scan-svg

--- ife/ConvenientStoreManager/client/build/main.js
+++ ife/ConvenientStoreManager/client/build/main.js
@@ -143,8 +143,8 @@ module.exports = require("regenerator-runtime");
 
 "use strict";
 /* WEBPACK VAR INJECTION */(function(__dirname) {Object.defineProperty(exports, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_D_Projects_Home_ife_ConvenientStoreManager_client_node_modules_babel_runtime_6_26_0_babel_runtime_regenerator__ = __webpack_require__(2);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_D_Projects_Home_ife_ConvenientStoreManager_client_node_modules_babel_runtime_6_26_0_babel_runtime_regenerator___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_D_Projects_Home_ife_ConvenientStoreManager_client_node_modules_babel_runtime_6_26_0_babel_runtime_regenerator__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_D_coding_Home_ife_ConvenientStoreManager_client_node_modules_babel_runtime_6_26_0_babel_runtime_regenerator__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_D_coding_Home_ife_ConvenientStoreManager_client_node_modules_babel_runtime_6_26_0_babel_runtime_regenerator___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_D_coding_Home_ife_ConvenientStoreManager_client_node_modules_babel_runtime_6_26_0_babel_runtime_regenerator__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_koa__ = __webpack_require__(4);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_koa___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_koa__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_nuxt__ = __webpack_require__(5);
@@ -156,11 +156,11 @@ module.exports = require("regenerator-runtime");
 
 
 var start = function () {
-  var _ref = _asyncToGenerator( /*#__PURE__*/__WEBPACK_IMPORTED_MODULE_0_D_Projects_Home_ife_ConvenientStoreManager_client_node_modules_babel_runtime_6_26_0_babel_runtime_regenerator___default.a.mark(function _callee2() {
+  var _ref = _asyncToGenerator( /*#__PURE__*/__WEBPACK_IMPORTED_MODULE_0_D_coding_Home_ife_ConvenientStoreManager_client_node_modules_babel_runtime_6_26_0_babel_runtime_regenerator___default.a.mark(function _callee2() {
     var _this = this;
 
     var app, host, port, config, nuxt, builder, options;
-    return __WEBPACK_IMPORTED_MODULE_0_D_Projects_Home_ife_ConvenientStoreManager_client_node_modules_babel_runtime_6_26_0_babel_runtime_regenerator___default.a.wrap(function _callee2$(_context2) {
+    return __WEBPACK_IMPORTED_MODULE_0_D_coding_Home_ife_ConvenientStoreManager_client_node_modules_babel_runtime_6_26_0_babel_runtime_regenerator___default.a.wrap(function _callee2$(_context2) {
       while (1) {
         switch (_context2.prev = _context2.next) {
           case 0:
@@ -191,8 +191,8 @@ var start = function () {
           case 10:
 
             app.use(function () {
-              var _ref2 = _asyncToGenerator( /*#__PURE__*/__WEBPACK_IMPORTED_MODULE_0_D_Projects_Home_ife_ConvenientStoreManager_client_node_modules_babel_runtime_6_26_0_babel_runtime_regenerator___default.a.mark(function _callee(ctx, next) {
-                return __WEBPACK_IMPORTED_MODULE_0_D_Projects_Home_ife_ConvenientStoreManager_client_node_modules_babel_runtime_6_26_0_babel_runtime_regenerator___default.a.wrap(function _callee$(_context) {
+              var _ref2 = _asyncToGenerator( /*#__PURE__*/__WEBPACK_IMPORTED_MODULE_0_D_coding_Home_ife_ConvenientStoreManager_client_node_modules_babel_runtime_6_26_0_babel_runtime_regenerator___default.a.mark(function _callee(ctx, next) {
+                return __WEBPACK_IMPORTED_MODULE_0_D_coding_Home_ife_ConvenientStoreManager_client_node_modules_babel_runtime_6_26_0_babel_runtime_regenerator___default.a.wrap(function _callee$(_context) {
                   while (1) {
                     switch (_context.prev = _context.next) {
                       case 0:
--- ife/ConvenientStoreManager/client/pages/scan.vue
+++ ife/ConvenientStoreManager/client/pages/scan.vue
@@ -1,7 +1,25 @@
 <template>
 <section class="container">
   <div class="view-port">
-    <div class="scanner-bg"></div>
+    <svg class="scanner-bg">
+      <g>
+          <path d="M0,0L640,0" style="fill:#000;opacity:0.75;stroke:#7f7f7f;stroke-width:280"/>
+          <path d="M640,140L640,340" style="fill:#000;opacity:0.75;stroke:#7f7f7f;stroke-width:440"/>
+          <path d="M640,480L0,480" style="fill:#000;opacity:0.75;stroke:#7f7f7f;stroke-width:280"/>
+          <path d="M0,340L0,140" style="fill:#000;opacity:0.75;stroke:#7f7f7f;stroke-width:440"/>
+          <rect fill="#000000" stroke-width="7" stroke-dashoffset="20" stroke-dasharray="40,160"   x="220" y="140"  width="200" height="200" id="svg_5" fill-opacity="0" stroke="#55f755"/>
+          <ellipse cx="320" cy="240" rx="150" ry="2" fill="url(#grad1)" >
+            <animate id="c1" attributeName="cy" attributeType="XML"  dur="4s" values="140;340;140" repeatCount="indefinite" opacity="0"/>
+          </ellipse>
+          <defs>
+            <radialGradient id="grad1" cx="50%" cy="50%" r="60%" fx="50%" fy="50%">
+              <stop offset="0%" style="stop-color:rgb(0,255,0);stop-opacity:1" />
+              <stop offset="100%" style="stop-color:rgb(255,255,255);stop-opacity:0" />
+            </radialGradient>
+            <animateTransform from="140" to="340" type="skewY" id=""/>
+          </defs>
+        </g>
+    </svg>
   </div>
   <div class=""> {{message}} </div>
 </section>
@@ -20,10 +38,8 @@
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 288px;
-  height: 288px;
-  border: 2px solid #3B8070;
-  opacity: .1;
+  width: 640px;
+  height: 480px;
 }
 </style>
 <script>
@@ -50,11 +66,8 @@ export default {
       quagga.start();
       quagga.onDetected((data) => {
         quagga.stop();
-
       })
-      quagga.onProcessed(function(result) {
-
-      });
+      quagga.onProcessed(function(result) {});
     })
   }
 }
